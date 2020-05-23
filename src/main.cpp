82ec3dd77e39c4401d243a1bf5e37c0f6660ebd0
Joe Withers <joewithers96@gmail.com>
2020-05-23 22:05:21 +0100

    clang-format pass

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ namespace SDL_GL_IMGUI_APP
 {
 	int SDLError(const char* msg)
 	{
-		std::cout << msg << SDL_GetError() << "\n";
+		std::cout<<msg<<SDL_GetError()<<"\n";
 		SDL_Quit();
 		return EXIT_FAILURE;
 	}
@@ -43,8 +43,7 @@ namespace SDL_GL_IMGUI_APP
 
 	SDL_GLContext createGLContext(SDL_Window* window)
 	{
-		// set opengl version to use in this program, 4.2 core profile (until I find a deprecated function to
-		// use)
+		// set opengl version to use in this program, 4.2 core profile (until I find a deprecated function to use)
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3); // :)
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
@@ -54,11 +53,11 @@ namespace SDL_GL_IMGUI_APP
 		// SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
 		return SDL_GL_CreateContext(window);
 	}
-} // namespace SDL_GL_IMGUI_APP
+}
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	std::cout << "Starting PointCloudRendering\n";
+	std::cout<<"Starting PointCloudRendering\n";
 
 	// Get command line arguments, we should really bail / print a help message here
 	const char* filepath = (argc > 1) ? argv[1] : "res/richmond-azaelias.ply";
@@ -71,8 +70,13 @@ int main(int argc, char* argv[])
 
 	// create window - TODO: RAII
 	SDL_Window* window = SDL_CreateWindow(
-		"OpenGL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT,
-		SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+		"OpenGL",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		DEFAULT_SCREEN_WIDTH,
+		DEFAULT_SCREEN_HEIGHT,
+		SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE
+	);
 
 	if (!window)
 	{
@@ -96,7 +100,7 @@ int main(int argc, char* argv[])
 	GLenum glewError = glewInit();
 	if (glewError != GLEW_OK)
 	{
-		std::cout << "GLEW cannot init with error: " << glewGetErrorString(glewError) << "\n";
+		std::cout<<"GLEW cannot init with error: "<<glewGetErrorString(glewError)<<"\n";
 		// manually destroy SDL stuff
 		SDL_GL_DeleteContext(glContext);
 		SDL_DestroyWindow(window);
@@ -107,8 +111,7 @@ int main(int argc, char* argv[])
 	// Init imgui
 	IMGUI_CHECKVERSION();
 	ImGuiContext* imGuiContext = ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
-	(void)io;
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
 
 	// Setup Dear ImGui style
 	ImGui::StyleColorsDark();
